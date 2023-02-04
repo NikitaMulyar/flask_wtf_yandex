c8553f744f0bc279b26d33fad9fdb6c47fa25d38
NikitaMulyar <nickm.school@mail.ru>
2023-02-04 14:25:36 +0300

    Задача Тренировке в полете + предыдущие

--- main.py
+++ main.py
@@ -10,5 +10,15 @@ def base(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    prof2 = prof
+    if 'строитель' in prof2:
+        prof2 = 'строитель'
+    elif 'инженер' in prof2:
+        prof2 = 'инженер'
+    return render_template('training.html', prof=prof2)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
