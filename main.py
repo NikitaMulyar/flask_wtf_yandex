ccf032ef5543c9ae15001136fa8bacc4bb133ad9
NikitaMulyar <ni***ol@mail.ru>
2023-02-06 00:48:12 +0300

    Задача Список профессий и предыдущие

--- main.py
+++ main.py
@@ -20,5 +20,13 @@ def training(prof):
     return render_template('training.html', prof=prof2)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    user_list = 'инженер-исследователь, пилот, строитель, экзобиолог, врач, инженер по терраформированию, \
+    климатолог, специалист по радиационной защите, астрогеолог, гляциолог, инженер жизнеобеспечения, \
+    метеоролог, оператор марсохода, киберинженер, штурман, пилот дронов'.split(', ')
+    return render_template('list_prof.html', list=list, user_list=user_list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
